bb5171cc92456045e6370094f091e5d93bdd6e5d
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-03-03 17:48:36 -0300

    addTodo, process date and findCurrentP

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,4 +1,4 @@
-// import
+import { projectList, saveToLocalStorage } from "./project.js";
 
 // CreateTodo factory function
 const CreateTodo = (todoProject, id, todo, completed, date) => {
@@ -60,9 +60,58 @@ const processTodoInput = (e) => {
 
     let todoProject =  findCurrentProject();
     let date = processDate(dateInput);
-    let todoId = idGenerator();
+    let todoId = id;
 
     // create new todo object and add to todo array -- with processed inputs
     const newTodo = CreateTodo(todoProject, todoId, todo, false, date);
+    projectList[todoProject].todoList.push(newTodo);
+    id = id + 1;
+    saveToLocalStorage();
+
+    // add todo to DOM
+    addTodo(todoProject, todoId, todo, false, date);
+    hideTodoForm();
+    e.preventDefault();
+}
+
+// process date input and set due date if not set
+const processDate = (date) => {
+    let formattedDate;
+    if (date === "") {
+        formattedDate = "No due date";
+    }
+    else {
+        formattedDate = date;
+    }
+    return formattedDate;
+}
+
+// display all todos in project
+const displayTodoList = (array) => {
+    const ul = document.querySelector("ul");
+    ul.textContent = "";
+    array.forEach((todo) => {
+        addTodo(todo.todoProject, todo.id, todo.todo, todo.completed, todo.date);
+    });
+}
+
+// create todo in DOM html
+const addTodo = (todoProject, todoId, todo, completed, date) => {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.id = todoId;
+    ul.appendChild(li);
+
+    const div = document.createElement("div");
+    div.classList.add("todo");
+    li.appendChild(div);
 
 }
+
+// find current project
+const findCurrentProject = () => {
+    let currentProject = document.querySelector(".currentProject");
+    return currentProject.dataset.project;
+}
+
+export { addEventList, processTodoInput, idGenerator, findCurrentProject, CreateTodo, displayTodoList, processDate, showTodoForm, hideTodoForm}
