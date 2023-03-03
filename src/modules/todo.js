00f58a72369d786a9e92a3769715dc11a7fc8f44
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-03-03 13:17:46 -0300

    CreateTodo factory + DOM ELs

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,28 +1,67 @@
-function createTodo(name, dueDate = 'No date') {
+// import
+
+// factory function
+const CreateTodo = (dataProject, id, todo, completed, date) => {
     return {
-        name,
-        dueDate,
-        description,
-        complete,
-        setName(name) {
-            this.name = name;
-        },
-        setDueDate(dueDate) {
-            this.dueDate = dueDate;
-        },
-        getDueDate() {
-            return this.dueDate;
-        },
-        getName() {
-            return this.name;
-        },
-        getDescription() {
-            return this.description;
-        },
-        getDateFormat() {
-            return this.dueDate.split('-').reverse().join('-');
-        }
+        dataProject,
+        id,
+        todo,
+        completed: completed,
+        date: date
     }
 }
 
-export { createTodo };
+// Add eventlisteners to DOM
+function addEventList() {
+    const addTodo = document.querySelector("#addTodo");
+    addTodo.addEventListener("click", showTodoForm);
+
+    const cancelTodo = document.querySelector("#todoCancelBtn");
+    cancelTodo.addEventListener("click", hideTodoForm);
+    cancelTodo.addEventListener("keyup", (e) => {
+        if (e.key === "Escape") {
+            hideTodoForm();
+        }
+    });
+
+    const submitTodo = document.querySelector("#todoSubmitBtn");
+    submitTodo.addEventListener("submit", processTodoInput);
+    submitTodo.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+            processTodoInput(e);
+        }
+    });
+}
+
+
+// popup add todo form
+const showTodoForm = () => {
+    const todoForm = document.querySelector("#todoForm");
+    todoForm.classList.remove("hidden");
+    document.getElementById('todoInput').focus();
+}
+
+// hide add-todo form
+const hideTodoForm = () => {
+    const todoForm = document.querySelector("#todoForm");
+    todoForm.classList.add("hidden");
+
+    // clear input fields
+    document.querySelector("#todoInput").value = "";
+    document.querySelector("#todoInputDate").value = "";
+}
+
+// process todo input
+const processTodoInput = (e) => {
+    e.preventDefault();
+
+    let todo = document.querySelector("#todoInput").value;
+    let dateInput = document.querySelector("#todoInputDate").value;
+
+    let todoProject =  findCurrentProject();
+    let date = processDate(dateInput);
+    let todoId = idGenerator();
+
+    // create new todo object and add to todo array -- with processed inputs
+    const newTodo = CreateTodo(todoProject, todoId, todo, false, date);
+}
