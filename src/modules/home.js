c90b7a5d04d424d9c7bc32f0e60e5f8216f6fda1
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-03-03 19:21:06 -0300

    main loading

---
+++ src/modules/home.js
@@ -0,0 +1,55 @@
+import { addDays, format, isEqual, isWithinInterval } from "date-fns";
+import parseISO from "date-fns/parseISO";
+import { projectList, hideAddTodoForm } from "./project";
+import { addTodo } from "./todo";
+
+// clear content
+const clearContent = () => {
+    const ul = document.querySelector('ul');
+    ul.textContent = '';
+}
+
+// display all todos
+const displayAllTodos = () => {
+    clearContent();
+    projectList.forEach((project) => {
+        project.todoList.forEach((todo) => {
+            addTodo(todo.todo, todo.description, todo.dueDate, todo.priority, todo.todoProject, todo.todoId);
+        });
+    });
+}
+
+// display todos for today
+const displayToday = () => {
+    clearContent();
+    projectList.forEach((project) => {
+        project.todoList.forEach((todo) => {
+            if (isEqual(parseISO(todo.dueDate), new Date())) {
+                addTodo(todo.todo, todo.description, todo.dueDate, todo.priority, todo.todoProject, todo.todoId);
+            }
+        });
+    });
+}
+
+// display todos for this next week
+const displayNextWeek = () => {
+    clearContent();
+    projectList.forEach((project) => {
+        project.todoList.forEach((todo) => {
+            if (isWithinInterval(parseISO(todo.dueDate), { start: new Date(), end: addDays(new Date(), 7) })) {
+                addTodo(todo.todo, todo.description, todo.dueDate, todo.priority, todo.todoProject, todo.todoId);
+            }
+        });
+    });
+}
+
+// check no todos
+const checkNoTodos = () => {
+    const ul = document.querySelector('ul');
+    const noTodos = document.createElement('p');
+    noTodos.classList.add('noTodos');
+    noTodos.textContent = 'No todos to display';
+    ul.appendChild(noTodos);
+}
+
+export { displayAllTodos, displayToday, displayNextWeek, checkNoTodos };
